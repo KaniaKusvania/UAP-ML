12234c159a949f2ee8777e73a5c3298df3b4f001
KaniaKusvania <149313463+KaniaKusvania@users.noreply.github.com>
2025-12-25 04:44:33 +0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 from PIL import Image
 import numpy as np
 import os
@@ -211,4 +211,5 @@ if uploaded_file and model is not None:
 # =============================
 st.markdown("---")
 st.caption("UAP Machine Learning • Klasifikasi Jenis Topi")
+
 
